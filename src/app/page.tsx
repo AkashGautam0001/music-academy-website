16d531372f95c86e5cdd3fb6b2d89a011a205288
Akash Gautam <akashgautam01584@gmail.com>
2024-03-29 12:57:32 +0530

    project complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonialCards from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-	return <h1 className="text-2xl text-center">Hello, This is Akash Gautam</h1>;
+	return (
+		<main className="min-h-5-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+			<HeroSection />
+			<FeaturedCourses />
+			<WhyChooseUs />
+			<MusicSchoolTestimonialCards />
+			<UpcomingWebinars />
+			<Instructors />
+			<Footer />
+		</main>
+	);
 }
